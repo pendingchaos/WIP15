98561d9cc90a99c7d5eec36f59f8d0296151e9eb
pendingchaos <pendingchaos02@gmail.com>
2015-11-23 22:49:09 +0000

    The trace is now displayed.

--- src/inspect_gui.c
+++ src/inspect_gui.c
@@ -1,9 +1,13 @@
 #include "libinspect.h"
 #include "libtrace.h"
+#include "glapi.h"
 
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <inttypes.h>
 
 static GtkWidget* main_window;
 
@@ -17,37 +21,157 @@ static const tree_item_t _children2[] = {{"Child 3", 0, NULL}};
 static const tree_item_t _children[] = {{"Child 1", 0, NULL}, {"Child 2", 1, _children2}};
 static const tree_item_t items[] = {{"Hello world!", 0, NULL}, {"Hello world again!", 2, _children}, {"Goodbye world!", 0, NULL}};
 
-static void populate_tree(GtkTreeStore* store,
-                          GtkTreeIter* parent,
-                          size_t count,
-                          const tree_item_t* items) {
+static const glapi_group_t* find_group(const char* name) {
+    for (size_t i = 0; i < glapi.group_count; i++) {
+        if (strcmp(glapi.groups[i]->name, name) == 0) {
+            return glapi.groups[i];
+        }
+    }
+    
+    return NULL;
+}
+
+static char* static_format(const char* format, ...) {
+    static char data[1024];
+    memset(data, 0, 1024);
+    
+    va_list list;
+    va_start(list, format);
+    vsnprintf(data, 1024, format, list);
+    va_end(list);
+    
+    return data;
+}
+
+static void format_value(char* str, trace_value_t value, const trace_t* trace) {
+    if (value.group_index < 0 ? false : (trace->group_names[value.group_index][0] != 0)) {
+        const glapi_group_t* group = find_group(trace->group_names[value.group_index]);
+        
+        if (!group) {
+        } else if (group->bitmask) {
+            //TODO
+        } else {
+            for (size_t i = 0; i < group->entry_count; i++) {
+                const glapi_group_entry_t *entry = group->entries[i];
+                uint64_t val = (value.type==Type_Boolean) ?
+                               *trace_get_bool(&value) :
+                               *trace_get_uint(&value);
+                if (entry->value == val) {
+                    strcat(str, static_format("%s(%zu)", entry->name, val));
+                    return;
+                }
+            }
+        }
+    }
+    
+    if (value.count > 1) strcat(str, static_format("["));
+    for (size_t i = 0; i < value.count; ++i) {
+        switch (value.type) {
+        case Type_Void: {
+            strcat(str, static_format("void"));
+            break;
+        }
+        case Type_UInt: {
+            strcat(str, static_format("%"PRIu64, trace_get_uint(&value)[i]));
+            break;
+        }
+        case Type_Int: {
+            strcat(str, static_format("%"PRId64, trace_get_int(&value)[i]));
+            break;
+        }
+        case Type_Double: {
+            strcat(str, static_format("%f", trace_get_double(&value)[i]));
+            break;
+        }
+        case Type_Boolean: {
+            strcat(str, static_format(trace_get_bool(&value)[i] ? "true" : "false"));
+            break;
+        }
+        case Type_Str: {
+            strcat(str, static_format("'%s'", trace_get_str(&value)[i]));
+            break;
+        }
+        case Type_Bitfield: {
+            strcat(str, static_format("%u", trace_get_bitfield(&value)[i]));
+            break;
+        }
+        case Type_FunctionPtr: {
+            strcat(str, static_format("<fn ptr>"));
+            break;
+        }
+        case Type_Ptr: {
+            strcat(str, static_format("0x%"PRIx64, trace_get_ptr(&value)[i]));
+            break;
+        }
+        case Type_Data: {
+            strcat(str, static_format("(data)"));
+            break;
+        }
+        }
+        
+        if (i != value.count-1) {
+            strcat(str, static_format(", "));
+        }
+    }
+    if (value.count > 1) strcat(str, static_format("]"));
+}
+
+static void format_command(char* str, inspect_command_t* command, const trace_t* trace) {
+    trace_command_t* trace_cmd = command->trace_cmd;
+    
+    strcat(str, static_format("%s(", command->name));
+    
+    size_t count = get_vec_size(command->trace_cmd->args) / sizeof(trace_arg_t);
     for (size_t i = 0; i < count; ++i) {
-        GtkTreeIter row;
-        gtk_tree_store_append(store, &row, parent);
-        gtk_tree_store_set(store, &row, 0, items[i].val, -1);
-        populate_tree(store, &row, items[i].child_count, items[i].children);
+        trace_arg_t* arg = ((trace_arg_t*)get_vec_data(command->trace_cmd->args)) + i;
+        
+        format_value(str, arg->val, trace);
+        if (i != count-1) {
+            strcat(str, static_format(", "));
+        }
+    }
+    
+    strcat(str, static_format(")"));
+    
+    if (trace_cmd->ret.type != Type_Void) {
+        strcat(str, static_format(" = "));
+        format_value(str, trace_cmd->ret, trace);
     }
 }
 
-static GtkWidget* create_tree(size_t count, const tree_item_t* items) {
-    GtkTreeStore* tree_store = gtk_tree_store_new(1, G_TYPE_STRING);
+static void init_trace_tree(GtkTreeView* tree,
+                            inspection_t* inspection) {
+    GtkTreeStore* store = gtk_tree_store_new(1, G_TYPE_STRING);
     
-    populate_tree(tree_store, NULL, count, items);
+    for (size_t i = 0; i < inspection->frame_count; ++i) {
+        char frame_str[1024];
+        memset(frame_str, 0, 1024);
+        snprintf(frame_str, 1024, "Frame %zu", i);
+        
+        GtkTreeIter frame_row;
+        gtk_tree_store_append(store, &frame_row, NULL);
+        gtk_tree_store_set(store, &frame_row, 0, frame_str, -1);
+        
+        inspect_frame_t* frame = inspection->frames + i;
+        for (size_t j = 0; j < frame->command_count; ++j) {
+            GtkTreeIter cmd_row;
+            gtk_tree_store_append(store, &cmd_row, &frame_row);
+            
+            inspect_command_t* cmd = frame->commands + j;
+            char cmd_str[1024];
+            memset(cmd_str, 0, 1024);
+            format_command(cmd_str, cmd, inspection->trace);
+            
+            gtk_tree_store_set(store, &cmd_row, 0, cmd_str, -1);
+        }
+    }
     
-    GtkWidget* tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(tree_store));
-    gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(tree), FALSE);
-    g_object_unref(G_OBJECT(tree_store));
+    gtk_tree_view_set_model(tree, GTK_TREE_MODEL(store));
     
     GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
-    GtkTreeViewColumn* column = gtk_tree_view_column_new();
+    GtkTreeViewColumn* column = gtk_tree_view_get_column(tree, 0);
     gtk_tree_view_column_pack_start(column, renderer, FALSE);
     gtk_tree_view_column_set_attributes(column, renderer, "text", 0, NULL);
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
-    
-    GtkWidget* window = gtk_scrolled_window_new(NULL, NULL);
-    gtk_container_add(GTK_CONTAINER(window), tree);
-    
-    return window;
 }
 
 void quit_callback(GObject* obj, gpointer user_data) {
@@ -87,18 +211,11 @@ int main(int argc, char** argv) {
     
     gtk_init(&argc, &argv);
     
-    /*GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "WIP15 Inspector");
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    
-    GtkWidget* tree = create_tree(3, items);
-    gtk_container_add(GTK_CONTAINER(window), tree);
-    
-    gtk_widget_show_all(window);*/
-    
     GtkBuilder* builder = gtk_builder_new_from_file("ui.glade");
     gtk_builder_connect_signals(builder, inspection);
+    
+    GObject* trace_view = gtk_builder_get_object(builder, "trace_view");
+    init_trace_tree(GTK_TREE_VIEW(trace_view), inspection);
     
     main_window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
     gtk_widget_show_all(main_window);
