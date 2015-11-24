2bc8eb7df9ec8bb44cd1929240d831a5e6d0cb8e
pendingchaos <pendingchaos02@gmail.com>
2015-11-24 15:36:13 +0000

    Attachment previews.

--- src/inspect_gui.c
+++ src/inspect_gui.c
@@ -12,6 +12,9 @@
 
 static GtkWidget* main_window;
 static GtkBuilder* builder;
+static GdkPixbuf* info_pixbuf;
+static GdkPixbuf* warning_pixbuf;
+static GdkPixbuf* error_pixbuf;
 
 static const glapi_group_t* find_group(const char* name) {
     for (size_t i = 0; i < glapi.group_count; i++) {
@@ -133,18 +136,40 @@ static void format_command(char* str, inspect_command_t* command, const trace_t*
 
 static void init_trace_tree(GtkTreeView* tree,
                             inspection_t* inspection) {
-    GtkTreeStore* store = gtk_tree_store_new(1, G_TYPE_STRING);
+    GtkTreeStore* store = gtk_tree_store_new(2, GDK_TYPE_PIXBUF, G_TYPE_STRING);
     
     for (size_t i = 0; i < inspection->frame_count; ++i) {
+        inspect_frame_t* frame = inspection->frames + i;
+        
         char frame_str[1024];
         memset(frame_str, 0, 1024);
         snprintf(frame_str, 1024, "Frame %zu", i);
         
+        bool error = false;
+        bool warning = false;
+        bool info = false;
+        for (size_t j = 0; j < frame->command_count; ++j) {
+            inspect_attachment_t* attachment = frame->commands[j].attachments;
+            while (attachment) {
+                error = error || attachment->type == AttachType_Error;
+                warning = warning || attachment->type == AttachType_Warning;
+                info = info || attachment->type == AttachType_Info;
+                attachment = attachment->next;
+            }
+        }
+        
+        GdkPixbuf* pixbuf = NULL;
+        if (error)
+            pixbuf = error_pixbuf;
+        else if (warning)
+            pixbuf = warning_pixbuf;
+        else if (info)
+            pixbuf = info_pixbuf;
+        
         GtkTreeIter frame_row;
         gtk_tree_store_append(store, &frame_row, NULL);
-        gtk_tree_store_set(store, &frame_row, 0, frame_str, -1);
+        gtk_tree_store_set(store, &frame_row, 0, pixbuf, 1, frame_str, -1);
         
-        inspect_frame_t* frame = inspection->frames + i;
         for (size_t j = 0; j < frame->command_count; ++j) {
             GtkTreeIter cmd_row;
             gtk_tree_store_append(store, &cmd_row, &frame_row);
@@ -154,16 +179,40 @@ static void init_trace_tree(GtkTreeView* tree,
             memset(cmd_str, 0, 1024);
             format_command(cmd_str, cmd, inspection->trace);
             
-            gtk_tree_store_set(store, &cmd_row, 0, cmd_str, -1);
+            bool error = false;
+            bool warning = false;
+            bool info = false;
+            inspect_attachment_t* attachment = cmd->attachments;
+            while (attachment) {
+                error = error || attachment->type == AttachType_Error;
+                warning = warning || attachment->type == AttachType_Warning;
+                info = info || attachment->type == AttachType_Info;
+                attachment = attachment->next;
+            }
+            
+            pixbuf = NULL;
+            if (error)
+                pixbuf = error_pixbuf;
+            else if (warning)
+                pixbuf = warning_pixbuf;
+            else if (info)
+                pixbuf = info_pixbuf;
+            
+            gtk_tree_store_set(store, &cmd_row, 0, pixbuf, 1, cmd_str, -1);
         }
     }
     
     gtk_tree_view_set_model(tree, GTK_TREE_MODEL(store));
     
-    GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
+    GtkCellRenderer* renderer = gtk_cell_renderer_pixbuf_new();
     GtkTreeViewColumn* column = gtk_tree_view_get_column(tree, 0);
     gtk_tree_view_column_pack_start(column, renderer, FALSE);
-    gtk_tree_view_column_set_attributes(column, renderer, "text", 0, NULL);
+    gtk_tree_view_column_set_attributes(column, renderer, "pixbuf", 0, NULL);
+    
+    renderer = gtk_cell_renderer_text_new();
+    column = gtk_tree_view_get_column(tree, 1);
+    gtk_tree_view_column_pack_start(column, renderer, FALSE);
+    gtk_tree_view_column_set_attributes(column, renderer, "text", 1, NULL);
 }
 
 void quit_callback(GObject* obj, gpointer user_data) {
@@ -196,7 +245,6 @@ void command_select_callback(GObject* obj, gpointer user_data) {
         assert(indices[1] < frame->command_count);
         inspect_command_t* cmd = frame->commands + indices[1];
         
-        printf("%s has been selected.\n", cmd->name);
         GObject* view = gtk_builder_get_object(builder, "selected_command_attachments");
         GtkTreeStore* store = GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(view)));
         gtk_tree_store_clear(store);
@@ -221,19 +269,53 @@ int main(int argc, char** argv) {
     
     trace_t* trace = load_trace(argv[1]);
     
-    trace_error_t error = get_trace_error();
-    if (error == TraceError_Invalid) {
-        fprintf(stderr, "Invalid trace file: %s\n", get_trace_error_desc());
-        return EXIT_FAILURE;
-    } else if (error == TraceError_UnableToOpen) {
-        fprintf(stderr, "Unable to open trace file.\n");
-        return EXIT_FAILURE;
+    {
+        trace_error_t error = get_trace_error();
+        if (error == TraceError_Invalid) {
+            fprintf(stderr, "Invalid trace file: %s\n", get_trace_error_desc());
+            return EXIT_FAILURE;
+        } else if (error == TraceError_UnableToOpen) {
+            fprintf(stderr, "Unable to open trace file.\n");
+            return EXIT_FAILURE;
+        }
     }
     
     inspection_t* inspection = create_inspection(trace);
     inspect(inspection);
     
     gtk_init(&argc, &argv);
+    
+    GtkIconTheme* icon_theme = gtk_icon_theme_new();
+    {
+        GError* error = NULL;
+        info_pixbuf = gtk_icon_theme_load_icon(icon_theme, "dialog-information", 16, 0, &error);
+        if (error) {
+            fprintf(stderr, "Unable to load information icon: %s\n", error->message);
+            g_error_free(error);
+            free_inspection(inspection);
+            free_trace(trace);
+            return EXIT_FAILURE;
+        }
+        warning_pixbuf = gtk_icon_theme_load_icon(icon_theme, "dialog-warning", 16, 0, &error);
+        if (error) {
+            fprintf(stderr, "Unable to load information icon: %s\n", error->message);
+            g_object_unref(info_pixbuf);
+            g_error_free(error);
+            free_inspection(inspection);
+            free_trace(trace);
+            return EXIT_FAILURE;
+        }
+        error_pixbuf = gtk_icon_theme_load_icon(icon_theme, "dialog-error", 16, 0, &error);
+        if (error) {
+            fprintf(stderr, "Unable to load information icon: %s\n", error->message);
+            g_object_unref(warning_pixbuf);
+            g_object_unref(info_pixbuf);
+            g_error_free(error);
+            free_inspection(inspection);
+            free_trace(trace);
+            return EXIT_FAILURE;
+        }
+    }
     
     builder = gtk_builder_new_from_file("ui.glade");
     gtk_builder_connect_signals(builder, inspection);
@@ -257,6 +339,11 @@ int main(int argc, char** argv) {
     gtk_main();
     
     g_object_unref(G_OBJECT(builder));
+    
+    g_object_unref(error_pixbuf);
+    g_object_unref(warning_pixbuf);
+    g_object_unref(info_pixbuf);
+    g_object_unref(icon_theme);
     
     free_inspection(inspection);
     free_trace(trace);
