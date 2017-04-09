d7638fa7a05429b47055b3945ffeb11477f0604b
pendingchaos <pendingchaos02@gmail.com>
2017-04-09 23:01:11 +0100

    Implement the initial state viewer for inspect-gui

--- src/gui/trace.c
+++ src/gui/trace.c
@@ -103,6 +103,271 @@ static void init_trace_tree(GtkTreeView* tree) {
     }
 }*/
 
+static uint8_t* get_state(trc_data_t* data) {
+    static uint8_t res[8192];
+    size_t srcsize = data->uncompressed_size;
+    void* ptr = trc_lock_data(data, true, false);
+    memcpy(res, ptr, srcsize<8192?srcsize:8192);
+    trc_unlock_data(data);
+    return res;
+}
+
+static bool* get_stateb(trc_data_t* data) {
+    return (bool*)get_state(data);
+}
+
+static float* get_statef(trc_data_t* data) {
+    return (float*)get_state(data);
+}
+
+static int* get_statei(trc_data_t* data) {
+    return (int*)get_state(data);
+}
+
+static uint* get_stateu(trc_data_t* data) {
+    return (uint*)get_state(data);
+}
+
+static size_t category_stack_size;
+static GtkTreeIter category_stack[64];
+
+static void begin_category(GtkTreeStore* store, const char* format, ...) {
+    va_list list;
+    va_start(list, format);
+    char name[1024];
+    vsnprintf(name, sizeof(name), format, list);
+    va_end(list);
+    
+    GtkTreeIter* parent = category_stack_size>0 ? &category_stack[category_stack_size-1] : NULL;
+    GtkTreeIter* row = &category_stack[category_stack_size++];
+    gtk_tree_store_append(store, row, parent);
+    gtk_tree_store_set(store, row, 0, name, 1, "", -1);
+}
+
+static void end_category() {
+    category_stack_size--;
+}
+
+static void value(GtkTreeStore* store, const char* name, const char* format, ...) {
+    va_list list;
+    va_start(list, format);
+    char value[1024];
+    vsnprintf(value, sizeof(value), format, list);
+    va_end(list);
+    
+    GtkTreeIter* parent = category_stack_size>0 ? &category_stack[category_stack_size-1] : NULL;
+    GtkTreeIter row;
+    gtk_tree_store_append(store, &row, parent);
+    gtk_tree_store_set(store, &row, 0, name, 1, value, -1);
+}
+
+static void init_state_tree(GtkTreeView* tree, const trc_gl_context_rev_t* ctx) {
+    GtkTreeStore* store = GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(tree)));
+    gtk_tree_store_clear(store);
+    
+    //TODO: The current context
+    value(store, "Drawable Size", static_format("%ux%u", ctx->drawable_width, ctx->drawable_height));
+    
+    begin_category(store, "Buffer Bindings");
+    #define BUFFER(type) value(store, #type, "%u", ctx->bound_buffer_##type);
+    BUFFER(GL_ARRAY_BUFFER);
+    BUFFER(GL_ELEMENT_ARRAY_BUFFER);
+    BUFFER(GL_UNIFORM_BUFFER);
+    BUFFER(GL_SHADER_STORAGE_BUFFER);
+    BUFFER(GL_ATOMIC_COUNTER_BUFFER);
+    BUFFER(GL_DRAW_INDIRECT_BUFFER);
+    BUFFER(GL_COPY_READ_BUFFER);
+    BUFFER(GL_COPY_WRITE_BUFFER);
+    BUFFER(GL_DISPATCH_INDIRECT_BUFFER);
+    BUFFER(GL_PIXEL_PACK_BUFFER);
+    BUFFER(GL_PIXEL_UNPACK_BUFFER);
+    BUFFER(GL_QUERY_BUFFER);
+    BUFFER(GL_TEXTURE_BUFFER);
+    BUFFER(GL_TRANSFORM_FEEDBACK_BUFFER);
+    end_category();
+    
+    #define STATE(prefix, name, getfunc, type, fmt, get) do {\
+        type* val = getfunc(ctx->prefix##_##name);\
+        size_t count = ctx->prefix##_##name->uncompressed_size/sizeof(uint);\
+        if (count == 1) {\
+            size_t i = 0;\
+            value(store, #name, fmt, get);\
+        } else {\
+            begin_category(store, #name); \
+            for (size_t i = 0; i < count; i++)\
+                value(store, static_format("%zu", i), fmt, get);\
+            end_category();\
+        }\
+    } while (0)
+    
+    #define STATE_FLOAT(prefix, name) STATE(prefix, name, get_statef, float, "%f", val[i])
+    #define STATE_UINT(prefix, name) STATE(prefix, name, get_stateu, uint, "%u", val[i])
+    #define STATE_INT(prefix, name) STATE(prefix, name, get_statei, int, "%d", val[i])
+    #define STATE_ENUM(prefix, group, name) STATE(prefix, name, get_stateu, uint, "%s", get_enum_str(group, val[i]))
+    #define STATE_BOOL(prefix, name) STATE(prefix, name, get_stateb, bool, "%s", val[i]?"true":"false")
+    
+    begin_category(store, "Query Bindings");
+    STATE_UINT(bound_queries, GL_SAMPLES_PASSED);
+    STATE_UINT(bound_queries, GL_ANY_SAMPLES_PASSED);
+    STATE_UINT(bound_queries, GL_ANY_SAMPLES_PASSED_CONSERVATIVE);
+    STATE_UINT(bound_queries, GL_PRIMITIVES_GENERATED);
+    STATE_UINT(bound_queries, GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN);
+    STATE_UINT(bound_queries, GL_TIME_ELAPSED);
+    end_category();
+    
+    begin_category(store, "Texture Bindings");
+    STATE_UINT(bound_textures, GL_TEXTURE_1D);
+    STATE_UINT(bound_textures, GL_TEXTURE_2D);
+    STATE_UINT(bound_textures, GL_TEXTURE_3D);
+    STATE_UINT(bound_textures, GL_TEXTURE_1D_ARRAY);
+    STATE_UINT(bound_textures, GL_TEXTURE_2D_ARRAY);
+    STATE_UINT(bound_textures, GL_TEXTURE_RECTANGLE);
+    STATE_UINT(bound_textures, GL_TEXTURE_CUBE_MAP);
+    STATE_UINT(bound_textures, GL_TEXTURE_CUBE_MAP_ARRAY);
+    STATE_UINT(bound_textures, GL_TEXTURE_BUFFER);
+    STATE_UINT(bound_textures, GL_TEXTURE_2D_MULTISAMPLE);
+    STATE_UINT(bound_textures, GL_TEXTURE_2D_MULTISAMPLE_ARRAY);
+    end_category();
+    
+    value(store, "GL_CURRENT_PROGRAM", static_format("%u", ctx->bound_program));
+    value(store, "GL_VERTEX_ARRAY_BINDING", static_format("%u", ctx->bound_vao));
+    value(store, "GL_RENDERBUFFER_BINDING", static_format("%u", ctx->bound_renderbuffer));
+    value(store, "GL_READ_FRAMEBUFFER_BINDING", static_format("%u", ctx->read_framebuffer));
+    value(store, "GL_DRAW_FRAMEBUFFER_BINDING", static_format("%u", ctx->draw_framebuffer));
+    
+    begin_category(store, "Enabled");
+    STATE_BOOL(enabled, GL_BLEND);
+    {
+        bool* val = get_stateb(ctx->enabled_GL_CLIP_DISTANCE0);
+        size_t count = ctx->enabled_GL_CLIP_DISTANCE0->uncompressed_size / sizeof(bool);
+        for (size_t i = 0; i < count; i++)
+            value(store, static_format("GL_CLIP_DISTANCE%zu", i), val[i]?"true":"false");
+    }
+    STATE_BOOL(enabled, GL_COLOR_LOGIC_OP);
+    STATE_BOOL(enabled, GL_CULL_FACE);
+    STATE_BOOL(enabled, GL_DEBUG_OUTPUT);
+    STATE_BOOL(enabled, GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    STATE_BOOL(enabled, GL_DEPTH_CLAMP);
+    STATE_BOOL(enabled, GL_DEPTH_TEST);
+    STATE_BOOL(enabled, GL_DITHER);
+    STATE_BOOL(enabled, GL_FRAMEBUFFER_SRGB);
+    STATE_BOOL(enabled, GL_LINE_SMOOTH);
+    STATE_BOOL(enabled, GL_MULTISAMPLE);
+    STATE_BOOL(enabled, GL_POLYGON_OFFSET_FILL);
+    STATE_BOOL(enabled, GL_POLYGON_OFFSET_LINE);
+    STATE_BOOL(enabled, GL_POLYGON_OFFSET_POINT);
+    STATE_BOOL(enabled, GL_POLYGON_SMOOTH);
+    STATE_BOOL(enabled, GL_PRIMITIVE_RESTART);
+    STATE_BOOL(enabled, GL_PRIMITIVE_RESTART_FIXED_INDEX);
+    STATE_BOOL(enabled, GL_RASTERIZER_DISCARD);
+    STATE_BOOL(enabled, GL_SAMPLE_ALPHA_TO_COVERAGE);
+    STATE_BOOL(enabled, GL_SAMPLE_ALPHA_TO_ONE);
+    STATE_BOOL(enabled, GL_SAMPLE_SHADING);
+    STATE_BOOL(enabled, GL_SAMPLE_MASK);
+    STATE_BOOL(enabled, GL_SCISSOR_TEST);
+    STATE_BOOL(enabled, GL_STENCIL_TEST);
+    STATE_BOOL(enabled, GL_TEXTURE_CUBE_MAP_SEAMLESS);
+    STATE_BOOL(enabled, GL_PROGRAM_POINT_SIZE);
+    end_category();
+    
+    begin_category(store, "Blending");
+    STATE_FLOAT(state_float, GL_BLEND_COLOR);
+    STATE_ENUM(state_enum, "BlendingFactorDest", GL_BLEND_DST_RGB);
+    STATE_ENUM(state_enum, "BlendingFactorDest", GL_BLEND_DST_ALPHA);
+    STATE_ENUM(state_enum, "BlendingFactorSrc", GL_BLEND_SRC_RGB);
+    STATE_ENUM(state_enum, "BlendingFactorSrc", GL_BLEND_SRC_ALPHA);
+    STATE_ENUM(state_enum, "BlendEquationMode", GL_BLEND_EQUATION_RGB);
+    STATE_ENUM(state_enum, "BlendEquationMode", GL_BLEND_EQUATION_ALPHA);
+    end_category();
+    
+    begin_category(store, "Stencil");
+    STATE_ENUM(state_enum, "StencilFunction", GL_STENCIL_FUNC);
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_FAIL); 
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_PASS_DEPTH_FAIL);
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_PASS_DEPTH_PASS);
+    STATE_ENUM(state_enum, "StencilFunction", GL_STENCIL_BACK_FUNC);
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_BACK_FAIL); 
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_BACK_PASS_DEPTH_FAIL);
+    STATE_ENUM(state_enum, "StencilOp", GL_STENCIL_BACK_PASS_DEPTH_PASS);
+    STATE_INT(state_int, GL_STENCIL_CLEAR_VALUE);
+    STATE_INT(state_int, GL_STENCIL_VALUE_MASK);
+    STATE_INT(state_int, GL_STENCIL_REF);
+    STATE_INT(state_int, GL_STENCIL_BACK_VALUE_MASK);
+    STATE_INT(state_int, GL_STENCIL_BACK_REF);
+    STATE_INT(state_int, GL_STENCIL_WRITEMASK);
+    STATE_INT(state_int, GL_STENCIL_BACK_WRITEMASK);
+    end_category();
+    
+    begin_category(store, "Pack");
+    STATE_BOOL(state_bool, GL_PACK_SWAP_BYTES);
+    STATE_BOOL(state_bool, GL_PACK_LSB_FIRST);
+    STATE_INT(state_int, GL_PACK_ROW_LENGTH);
+    STATE_INT(state_int, GL_PACK_IMAGE_HEIGHT);
+    STATE_INT(state_int, GL_PACK_SKIP_ROWS);
+    STATE_INT(state_int, GL_PACK_SKIP_PIXELS);
+    STATE_INT(state_int, GL_PACK_SKIP_IMAGES);
+    STATE_INT(state_int, GL_PACK_ALIGNMENT);
+    end_category();
+    
+    begin_category(store, "Unpack");
+    STATE_BOOL(state_bool, GL_UNPACK_SWAP_BYTES);
+    STATE_BOOL(state_bool, GL_UNPACK_LSB_FIRST);
+    STATE_INT(state_int, GL_UNPACK_ROW_LENGTH);
+    STATE_INT(state_int, GL_UNPACK_IMAGE_HEIGHT);
+    STATE_INT(state_int, GL_UNPACK_SKIP_ROWS);
+    STATE_INT(state_int, GL_UNPACK_SKIP_PIXELS);
+    STATE_INT(state_int, GL_UNPACK_SKIP_IMAGES);
+    STATE_INT(state_int, GL_UNPACK_ALIGNMENT);
+    end_category();
+    
+    STATE_BOOL(state_bool, GL_DEPTH_WRITEMASK);
+    STATE_BOOL(state_bool, GL_COLOR_WRITEMASK);
+    STATE_BOOL(state_bool, GL_SAMPLE_COVERAGE_INVERT);
+    
+    STATE_INT(state_int, GL_PRIMITIVE_RESTART_INDEX);
+    STATE_INT(state_int, GL_SCISSOR_BOX); //TODO: Improve this
+    
+    STATE_ENUM(state_enum, "PolygonMode", GL_POLYGON_MODE);
+    STATE_ENUM(state_enum, "CullFaceMode", GL_CULL_FACE_MODE);
+    STATE_ENUM(state_enum, "FrontFaceDirection", GL_FRONT_FACE);
+    STATE_ENUM(state_enum, "DepthFunction", GL_DEPTH_FUNC);
+    STATE_ENUM(state_enum, "LogicOp", GL_LOGIC_OP_MODE);
+    STATE_ENUM(state_enum, NULL, GL_POINT_SPRITE_COORD_ORIGIN); //TODO: The group
+    STATE_ENUM(state_enum, NULL, GL_PROVOKING_VERTEX); //TODO: The group
+    
+    STATE_FLOAT(state_float, GL_VIEWPORT);
+    STATE_FLOAT(state_float, GL_DEPTH_CLEAR_VALUE);
+    STATE_FLOAT(state_float, GL_COLOR_CLEAR_VALUE);
+    STATE_FLOAT(state_float, GL_DEPTH_RANGE);
+    STATE_FLOAT(state_float, GL_POINT_SIZE);
+    STATE_FLOAT(state_float, GL_LINE_WIDTH);
+    STATE_FLOAT(state_float, GL_POLYGON_OFFSET_UNITS);
+    STATE_FLOAT(state_float, GL_POLYGON_OFFSET_FACTOR);
+    STATE_FLOAT(state_float, GL_SAMPLE_COVERAGE_VALUE);
+    STATE_FLOAT(state_float, GL_POINT_FADE_THRESHOLD_SIZE);
+    
+    begin_category(store, "Hints");
+    #define HINT(name) value(store, #name, "%s", get_enum_str("HintMode", ctx->hints_##name));
+    HINT(GL_FRAGMENT_SHADER_DERIVATIVE_HINT);
+    HINT(GL_LINE_SMOOTH_HINT);
+    HINT(GL_POLYGON_SMOOTH_HINT);
+    HINT(GL_TEXTURE_COMPRESSION_HINT);
+    #undef HINT
+    end_category();
+    
+    begin_category(store, "Capabilities");
+    STATE_INT(state_int, GL_MAX_CLIP_DISTANCES);
+    STATE_INT(state_int, GL_MAX_VIEWPORTS);
+    STATE_INT(state_int, GL_MAX_DRAW_BUFFERS);
+    end_category();
+    
+    #undef STATE_BOOL
+    #undef STATE_ENUM
+    #undef STATE_INT
+    #undef STATE_UINT
+    #undef STATE_FLOAT
+}
+
 void command_select_callback(GObject* obj, gpointer user_data) {
     GtkTreePath* path;
     gtk_tree_view_get_cursor(GTK_TREE_VIEW(obj), &path, NULL);
@@ -140,9 +405,10 @@ void command_select_callback(GObject* obj, gpointer user_data) {
             attachment = attachment->next;
         }
         
-        //TODO
-        /*init_state_tree(GTK_TREE_VIEW(gtk_builder_get_object(builder, "state_treeview")),
-                        &cmd->state);*/
+        uint64_t fake = trc_lookup_current_fake_gl_context(trace, revision);
+        const trc_gl_context_rev_t* state = trc_lookup_gl_context(trace, revision, fake);
+        
+        init_state_tree(GTK_TREE_VIEW(gtk_builder_get_object(builder, "state_treeview")), state);
     } else {
         GtkTreeView* tree = GTK_TREE_VIEW(gtk_builder_get_object(builder, "state_treeview"));
         GtkTreeStore* store = GTK_TREE_STORE(gtk_tree_view_get_model(tree));
