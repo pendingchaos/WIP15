5ad20796094fdab597e77d162069258980cdc450
pendingchaos <pendingchaos02@gmail.com>
2018-03-05 18:26:46 +0000

    Fix transform feedback tests with mesa's glsl compiler

--- src/testing/tests/transformfeedback.c
+++ src/testing/tests/transformfeedback.c
@@ -18,7 +18,7 @@ GLuint create_tf_program(const char* vert, bool separable) {
     GLuint program = glCreateProgram();
     glAttachShader(program, vertex);
     
-    glTransformFeedbackVaryings(program, 1, (const char*[]){"output"}, GL_INTERLEAVED_ATTRIBS);
+    glTransformFeedbackVaryings(program, 1, (const char*[]){"output_"}, GL_INTERLEAVED_ATTRIBS);
     
     glProgramParameteri(program, GL_PROGRAM_SEPARABLE, separable);
     glLinkProgram(program);
@@ -33,16 +33,16 @@ GLuint create_tf_program(const char* vert, bool separable) {
 
 #define TRANSFORMFEEDBACK_SETUP(buffer_size, buffer_data, vertex) do {\
     program = create_tf_program("#version 150\n"\
-        "in vec4 input;\n"\
-        "out vec4 output;\n"\
+        "in vec4 input_;\n"\
+        "out vec4 output_;\n"\
         "void main() {\n"\
         vertex\
         "}\n", false);\
     glUseProgram(program);\
     glBindBuffer(GL_ARRAY_BUFFER, buf);\
     glBufferData(GL_ARRAY_BUFFER, buffer_size*16, buffer_data, GL_STATIC_DRAW);\
-    glEnableVertexAttribArray(glGetAttribLocation(program, "input"));\
-    glVertexAttribPointer(glGetAttribLocation(program, "input"),\
+    glEnableVertexAttribArray(glGetAttribLocation(program, "input_"));\
+    glVertexAttribPointer(glGetAttribLocation(program, "input_"),\
                           4, GL_FLOAT, GL_FALSE, 0, (const GLvoid*)0);\
     glBindBuffer(GL_ARRAY_BUFFER, obuf);\
     glBufferData(GL_ARRAY_BUFFER, buffer_size*16, NULL, GL_STATIC_READ);\
@@ -53,8 +53,8 @@ GLuint create_tf_program(const char* vert, bool separable) {
 
 #define TRANSFORMFEEDBACK_SETUP_PIPELINE(buffer_size, buffer_data, vertex) do {\
     program = create_tf_program("#version 150\n"\
-        "in vec4 input;\n"\
-        "out vec4 output;\n"\
+        "in vec4 input_;\n"\
+        "out vec4 output_;\n"\
         "void main() {\n"\
         vertex\
         "}\n", true);\
@@ -62,8 +62,8 @@ GLuint create_tf_program(const char* vert, bool separable) {
     glUseProgramStages(pipeline, GL_VERTEX_SHADER_BIT, program);\
     glBindBuffer(GL_ARRAY_BUFFER, buf);\
     glBufferData(GL_ARRAY_BUFFER, buffer_size*16, buffer_data, GL_STATIC_DRAW);\
-    glEnableVertexAttribArray(glGetAttribLocation(program, "input"));\
-    glVertexAttribPointer(glGetAttribLocation(program, "input"),\
+    glEnableVertexAttribArray(glGetAttribLocation(program, "input_"));\
+    glVertexAttribPointer(glGetAttribLocation(program, "input_"),\
                           4, GL_FLOAT, GL_FALSE, 0, (const GLvoid*)0);\
     glBindBuffer(GL_ARRAY_BUFFER, obuf);\
     glBufferData(GL_ARRAY_BUFFER, buffer_size*16, NULL, GL_STATIC_READ);\
@@ -90,7 +90,7 @@ void transformfeedback_cleanup() {
 
 BEGIN_TEST(basic_0)
     TRANSFORMFEEDBACK_SETUP(2, DATA(1, 2, 3, 4, 5, 6, 7, 8),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 2);
     glEndTransformFeedback();
     assert_properties(GL_BUFFER, obuf, "data", 32,
@@ -99,7 +99,7 @@ END_TEST(basic_0, 320)
 
 BEGIN_TEST(basic_1)
     TRANSFORMFEEDBACK_SETUP_PIPELINE(2, DATA(1, 2, 3, 4, 5, 6, 7, 8),
-                                     "output = input * 2.0;");
+                                     "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 2);
     glEndTransformFeedback();
     assert_properties(GL_BUFFER, obuf, "data", 32,
@@ -108,7 +108,7 @@ END_TEST(basic_1, 320)
 
 BEGIN_TEST(error_0)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, 16, DATA(0, 0, 0, 0));
     assert_attachment("Buffer should not be modified as it is a transform feedback one while transform feedback is active and unpaused");
@@ -117,7 +117,7 @@ END_TEST(error_0, 320)
 
 BEGIN_TEST(error_1)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glBufferData(GL_TRANSFORM_FEEDBACK_BUFFER, 16, DATA(0, 0, 0, 0), GL_STATIC_DRAW);
     assert_attachment("Buffer should not be modified as it is a transform feedback one while transform feedback is active and unpaused");
@@ -126,7 +126,7 @@ END_TEST(error_1, 320)
 
 BEGIN_TEST(error_2)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glMapBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, GL_READ_ONLY);
     assert_attachment("Buffer should not be mapped as it is a transform feedback one while transform feedback is active and unpaused");
@@ -135,7 +135,7 @@ END_TEST(error_2, 320)
 
 /*BEGIN_TEST(error_3) //TODO: Check not yet implemented
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glBindBuffer(GL_PIXEL_PACK_BUFFER, obuf);
     float pixel[4];
@@ -146,7 +146,7 @@ END_TEST(error_3, 320)*/
 
 BEGIN_TEST(error_4)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glUseProgram(0);
     assert_attachment("The program binding cannot be modified while transform feedback is active and unpaused");
@@ -155,7 +155,7 @@ END_TEST(error_4, 320)
 
 BEGIN_TEST(error_5)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glLinkProgram(program);
     assert_attachment("The bound program cannot be modified while transform feedback is active and unpaused");
@@ -164,7 +164,7 @@ END_TEST(error_5, 320)
 
 BEGIN_TEST(error_6)
     TRANSFORMFEEDBACK_SETUP(1, DATA(1, 2, 3, 4),
-                            "output = input * 2.0;");
+                            "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glBindProgramPipeline(0);
     assert_attachment("The program pipeline binding cannot be modified while transform feedback is active and unpaused");
@@ -173,7 +173,7 @@ END_TEST(error_6, 320)
 
 BEGIN_TEST(error_7)
     TRANSFORMFEEDBACK_SETUP_PIPELINE(1, DATA(1, 2, 3, 4),
-                                     "output = input * 2.0;");
+                                     "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glUseProgramStages(pipeline, GL_VERTEX_SHADER_BIT, 0);
     assert_attachment("The bound program pipeline object cannot be modified while transform feedback is active and unpaused");
@@ -183,7 +183,7 @@ END_TEST(error_7, 320)
 /*BEGIN_TEST(error_8) //TODO: Should this raise an error?
 //Mesa doesn't seem to
     TRANSFORMFEEDBACK_SETUP_PIPELINE(1, DATA(1, 2, 3, 4),
-                                     "output = input * 2.0;");
+                                     "output_ = input_ * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glLinkProgram(program);
     assert_attachment("A used program cannot be modified while transform feedback is active and unpaused");
@@ -192,7 +192,7 @@ END_TEST(error_8, 320)*/
 
 BEGIN_TEST(error_9)
     TRANSFORMFEEDBACK_SETUP_PIPELINE(1, DATA(1, 2, 3, 4),
-                                     "output = input * 2.0;");
+                                     "output_ = input * 2.0;");
     glDrawArrays(GL_POINTS, 0, 1);
     glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, 0);
     assert_attachment("Cannot modify GL_TRANSFORM_FEEDBACK_BUFFER when transform feedback is active and not paused");
@@ -203,7 +203,7 @@ BEGIN_TEST(transform_feedback_object_0)
 //does not work because of a Nvidia driver bug
 //https://stackoverflow.com/questions/39501309/buffer-bindings-of-transform-feedback-objects
     /*TRANSFORMFEEDBACK_SETUP(2, DATA(1, 2, 3, 4, 5, 6, 7, 8),
-                            "output = input * 2.0;");
+                            "output_ = input * 2.0;");
     glEndTransformFeedback();
     
     GLuint obuf2;
